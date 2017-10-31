5e4acf199e5789ca4f81c9f1c9810505cf74a747
David Snopek <dsnopek@gmail.com>
2017-10-30 23:30:54 -0500

    CRM-21372: Fix fatal error adding profile on Drupal 8

--- CRM/Utils/System/Drupal8.php
+++ CRM/Utils/System/Drupal8.php
@@ -547,6 +547,23 @@ class CRM_Utils_System_Drupal8 extends CRM_Utils_System_DrupalBase {
   /**
    * @inheritDoc
    */
+  public function isPasswordUserGenerated() {
+    if (\Drupal::config('user.settings')->get('verify_mail') == TRUE) {
+      return FALSE;
+    }
+    return TRUE;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function updateCategories() {
+    // @todo Is anything necessary?
+  }
+
+  /**
+   * @inheritDoc
+   */
   public function getLoggedInUfID() {
     if ($id = \Drupal::currentUser()->id()) {
       return $id;
