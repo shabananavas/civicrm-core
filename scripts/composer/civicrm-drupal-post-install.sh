d622637a3827b72d1abda464ea40c7112d64f026
Shabana Navas <desam9@hotmail.com>
2018-05-24 17:31:42 +0530

    Add post install script handler to download and move the necessary assets.

--- scripts/composer/civicrm-drupal-post-install.sh
+++ scripts/composer/civicrm-drupal-post-install.sh
@@ -1,14 +1,13 @@
 # Download and install all assets.
-cd vendor/civicrm/civicrm-core
-bower install --allow-root
-cd ../../../
-# Move the necessary assets to the libraries directory.
 asset_source=./vendor/civicrm/civicrm-core
-asset_dest=./web/libraries/civicrm
+asset_dest=$2
+cd $asset_source
+bower install --allow-root
+# Move back to the project home directory.
+cd $1
+# Move the necessary assets to the libraries directory.
 mkdir -p $asset_dest
 rsync -mr --include='*.'{html,js,css,svg,png,jpg,jpeg,ico,gif,woff,woff2,ttf,eot} --include='*/' --exclude='*' $asset_source/ $asset_dest/
 rm -rf $asset_dest/tests
 cp -r $asset_source/extern $asset_dest/
 cp $asset_source/civicrm.config.php $asset_dest/
-# Make sure we have permission to add the civicrm.settings.php file.
-chmod 0775 web/sites/default
